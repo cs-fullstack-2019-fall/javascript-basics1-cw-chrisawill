0b80664e016093cea2414894eba7c2792d1db4f8
Christian Williams <chrisawill05@gmail.com>
2019-08-06 16:10:20 -0500

    fixed name variable and caculated dog's age

---
+++ index.js
@@ -0,0 +1,14 @@
+//Problem 1 Print "Hello World"
+
+//Problem 2 Create a variable called nameVariable. Print "Hello " + nameVariable
+
+//Problem 3 Create a variable named dogAge. Print "Your dog is: " + dogAge * 7
+
+//roblem 4 Print "1 + 1 = " and the answe
+
+alert("Hello World");
+var nameChris= "christian";
+alert("Hello " + nameChris);
+var dogAge= 4;
+alert("Your dog's age is " + dogAge * 7);
+alert("1+1= " + 2);
